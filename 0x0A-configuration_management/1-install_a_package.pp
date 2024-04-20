78a27d1804300bd291f2d77ec58729a4a0d2fe90
Pilate <pilatech6@gmail.com>
2024-04-20 04:51:48 +0200

    fix #1 of 0x0A

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
 # Install flask package
 
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  name     => 'werkzeug',
+  provider => pip3,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
+  name     => 'flask',
   provider => pip3,
+  require  => Package['werkzeug']
 }
