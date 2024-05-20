198897e5cdf5867c0410aef7615c1f422631fb5a
Pilate <pilatech6@gmail.com>
2024-05-20 20:11:28 +0200

    edit_3 #0 of 0x15

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,11 +2,11 @@
 """This script makes an API request and display data"""
 
 if __name__ == "__main__":
-    from sys import argv
     import json
-
     import requests
-    user_id = argv[1]
+    import sys
+
+    user_id = sys.argv[1]
     todo_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
     todo_req = requests.get(todo_url)
     todos = json.loads(todo_req.text)
@@ -18,10 +18,10 @@ if __name__ == "__main__":
     completed = []
     total = len(todos)
     for i in todos:
-        if(i.get('completed')):
+        if (i.get('completed')):
             completed.append(i.get('title'))
 
     done = len(completed)
     print(f"Employee {user.get('name')} is done with tasks({done}/{total}):")
     for todo in completed:
-        print(f"\t{todo}")
+        print(f"    {todo}")
