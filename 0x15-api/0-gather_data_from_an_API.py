6e45599c2d5f513eedc731e9fbed441ebf3775c2
Pilate <pilatech6@gmail.com>
2024-05-20 20:06:25 +0200

    edit_2 #0 of 0x15

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """This script makes an API request and display data"""
-from sys import argv
-import json
 
-import requests
-user_id = argv[1]
-todo_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-todo_req = requests.get(todo_url)
-todos = json.loads(todo_req.text)
+if __name__ == "__main__":
+    from sys import argv
+    import json
 
+    import requests
+    user_id = argv[1]
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    todo_req = requests.get(todo_url)
+    todos = json.loads(todo_req.text)
 
-user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-user_req = requests.get(user_url)
-user = json.loads(user_req.text)
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    user_req = requests.get(user_url)
+    user = json.loads(user_req.text)
 
-completed = []
-total = len(todos)
-for i in todos:
-    if(i.get('completed')):
-        completed.append(i.get('title'))
+    completed = []
+    total = len(todos)
+    for i in todos:
+        if(i.get('completed')):
+            completed.append(i.get('title'))
 
-done = len(completed)
-print(f"Employee {user.get('name')} is done with tasks({done}/{total}):")
-for todo in completed:
-    print(f"\t{todo}")
+    done = len(completed)
+    print(f"Employee {user.get('name')} is done with tasks({done}/{total}):")
+    for todo in completed:
+        print(f"\t{todo}")
