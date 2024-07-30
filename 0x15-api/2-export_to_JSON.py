a53edf9b9c13e944444362309f3b0323d8efcd52
Pilate <pilatech6@gmail.com>
2024-07-30 03:54:27 +0200

    add #2 of 0x15

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module for getting todo employee data into JSON
+It takes employee ID as an argument and works out their todos
+"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        cid = sys.argv[1]
+        user_url = f"https://jsonplaceholder.typicode.com/users/{cid}"
+        r = requests.get(user_url)
+        employee = r.json()
+        eid = employee.get('id')
+        todos_url = "https://jsonplaceholder.typicode.com/todos"
+        r = requests.get(todos_url)
+        all_todos = r.json()
+        user_todos = []
+        done = []
+        for todo in all_todos:
+            if todo.get('userId') == int(cid):
+                user_todos.append(todo)
+        employee_JSON = '{"' + str(eid) + '": ['
+        todos_s = len(user_todos)
+        for i in range(todos_s):
+            line = '{"task": "' + user_todos[i].get('title') + '", '
+            completed = str(user_todos[i].get('completed')).lower()
+            line += '"completed": ' + completed + ', '
+            line += '"username": "' + employee.get('username')
+            line += '"}'
+            if i != todos_s - 1:
+                line += ', '
+            employee_JSON += line
+        employee_JSON += ']}'
+        print(employee_JSON)
+        with open(f"{eid}.JSON", 'w') as f:
+            f.write(employee_JSON)
